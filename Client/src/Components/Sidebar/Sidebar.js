349ed4d0e14810da8e9a65e7b3f0cd302488b59e
nourhan hasan <nourhan.m.hasan@gmail.com>
2024-04-03 17:14:04 +0300

    re-location of Sidebar components

---
+++ Client/src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption/SidebarOption";
+import {
+    Home,
+    MailOutline,
+    NotificationsNone,
+    PermIdentity,
+    Search,
+    Logout
+} from "@mui/icons-material"
+import { Button } from "@mui/material";
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <img src="./TrekkersIcon.jpg" alt="" className="sidebar__twitterIcon" />
+            <SidebarOption active Icon={Home} text="Home" />
+            <SidebarOption Icon={Search} text="Explore" />
+            <SidebarOption Icon={NotificationsNone} text="Notifications" />
+            <SidebarOption Icon={MailOutline} text="Messages" />
+            <SidebarOption Icon={PermIdentity} text="Profile" />
+            <SidebarOption Icon={Logout} text="Logout" path=''/>
+
+            <Button className="sidebar__tweet-btn" variant="outlined" fullWidth>
+                Post
+            </Button>
+        </div>
+    );
+};
+
+export default Sidebar;
