1f2251b06e9128c9ad35026d778e273a8066b5e4
nourhan hasan <nourhan.m.hasan@gmail.com>
2024-04-09 00:49:03 +0300

    add CRUD operations for user

--- Server/controllers/users-controller.js
+++ Server/controllers/users-controller.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user-model');
+const Country = require('../models/country-model');
+const Post = require('../models/post-model');
+const mongoose = require("mongoose");
 
 const login = (async(req ,res ,next) => {
     var payload = req.body;
@@ -132,9 +135,180 @@ const logout = (async(req, res, next)=>{
     });
 });
 
+const get_profile = (async(req, res, next) => {
+    try{
+        const userId = req.params.userId;
+        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        const user = await User.findById(userId);
+        if(!user){
+            const error = 'User not found';
+            return next(error);
+        }
+        const data = {
+            name: user.firstName + " " +user.lastName,
+            handle: user.username,
+            photo: user.profilePic,
+            followers: user.followers.length,
+            following: user.following.length,
+            bio: user.bio
+        }
+        res.status(200).json({ status: 'SUCCESS', data: data });
+    }catch(error){
+        next(error);
+    }
+});
+
+const edit_profile = (async(req, res, next) => {
+    try{
+        const userId = req.params.userId;
+        const { firstName, lastName, profilePic, bio } = req.body;
+        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        const updated_user = await User.findByIdAndUpdate(userId, {
+            firstName,
+            lastName,
+            profilePic,
+            bio
+        }, { new: true });
+        if(!updated_user){
+            const error = 'User not found';
+            return next(error);
+        }
+        res.status(200).json({ status: 'SUCCESS', data: updated_user });
+    }catch(error){
+        next(error);
+    }
+});
+
+const delete_profile = (async(req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        await Post.deleteMany({ postedBy: userId });
+        await Country.deleteMany({ postedBy: userId });
+        const deleted_user = await User.findByIdAndDelete(userId);
+        if (!deleted_user) {
+            const error = 'User not found';
+            return next(error);
+        }
+        res.status(200).json({ status: 'SUCCESS', message: 'User deleted successfully' });
+    } catch (error) {
+        next(error);
+    }
+});
+
+const follow = (async(req, res, next) => {
+    try {
+        const { followerId, followeeId } = req.body;
+        if (!mongoose.Types.ObjectId.isValid(followerId) || !mongoose.Types.ObjectId.isValid(followeeId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        const follower = await User.findById(followerId);
+        const followee = await User.findById(followeeId);
+        if (!follower || !followee) {
+            const error = 'Follower or followee not found';
+            return next(error);
+        }
+        if (follower.following.includes(followeeId) || followee.followers.includes(followerId)) {
+            const error = 'Follow relationship already exists';
+            return next(error);
+        }
+        follower.following.push(followeeId);
+        followee.followers.push(followerId);
+        await Promise.all([follower.save(), followee.save()]);
+        res.status(200).json({ status: 'SUCCESS', message: 'Follow relationship added successfully' });
+    } catch (error) {
+        next(error);
+    }
+});
+
+const unfollow = (async(req, res, next) => {
+    try {
+        const { followerId, followeeId } = req.body;
+        if (!mongoose.Types.ObjectId.isValid(followerId) || !mongoose.Types.ObjectId.isValid(followeeId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        const follower = await User.findById(followerId);
+        const followee = await User.findById(followeeId);
+        if (!follower || !followee) {
+            const error = 'Follower or followee not found';
+            return next(error);
+        }
+        if (!follower.following.includes(followeeId) || !followee.followers.includes(followerId)) {
+            const error = 'Follow relationship does not exist';
+            return next(error);
+        }
+        // Remove followeeId from follower's following list and followerId from followee's followers list
+        follower.following = follower.following.filter(id => id.toString() !== followeeId.toString());
+        followee.followers = followee.followers.filter(id => id.toString() !== followerId.toString());
+
+        await Promise.all([follower.save(), followee.save()]);
+
+        res.status(200).json({ status: 'SUCCESS', message: 'Unfollowed successfully' });
+    } catch (error) {
+        next(error);
+    }
+});
+
+const get_following = (async(req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        const user = await User.findById(userId);
+        if (!user) {
+            const error = 'User not found';
+            return next(error);
+        }
+        const following = await User.find({ _id: { $in: user.following } }, '_id firstName lastName profilePic username');
+
+        res.status(200).json({ status: 'SUCCESS', data: following });
+    } catch (error) {
+        next(error);
+    }
+});
+
+const get_followers = (async(req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+            const error = 'Invalid user ID';
+            return next(error);
+        }
+        const user = await User.findById(userId);
+        if (!user) {
+            const error = 'User not found';
+            return next(error);
+        }
+        const followers = await User.find({ _id: { $in: user.followers } }, '_id firstName lastName profilePic username');
+
+        res.status(200).json({ status: 'SUCCESS', data: followers });
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = {
     register,
     login,
-    logout
+    logout,
+    get_profile,
+    edit_profile,
+    delete_profile,
+    follow,
+    unfollow,
+    get_following,
+    get_followers
 };
--- Server/routes/user-route.js
+++ Server/routes/user-route.js
@@ -12,9 +12,6 @@ app.use(cors());
 router.route('/login')
         .post(users_controller.login)
         .get(cors())
-        /*.get((req, res)=>{
-            res.status(200).render("login");
-        })*/
 
 router.route('/register')
         .post(users_controller.register)
@@ -24,6 +21,23 @@ router.route('/register')
 
 router.route('/logout')
         .get(users_controller.logout);
+ 
+router.route('/api/users/:userId')
+        .get(users_controller.get_profile)
+        .put(users_controller.edit_profile)
+        .delete(users_controller.delete_profile)
+        
+router.route('/api/users/:userId/follow')
+        .post(users_controller.follow)
+
+router.route('/api/users/:userId/unfollow')
+        .delete(users_controller.unfollow)
+ 
+router.route('/api/users/:userId/following')
+        .get(users_controller.get_following)
+
+router.route('/api/users/:userId/followers')
+        .get(users_controller.get_followers)
 
 module.exports = router;
 
